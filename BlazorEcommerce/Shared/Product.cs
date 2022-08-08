ef574749a7c3a4521f585c2b8726f90c968b5f58
Saieshen Bhagwandin <SaieshenB@hollywoodbets.net>
2022-08-08 14:51:35 +0200

    Add project files.

---
+++ BlazorEcommerce/Shared/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorEcommerce.Shared
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;   
+
+        public string Description { get; set; } = string.Empty;
+
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+
+
+
+
+
+    }
+}
