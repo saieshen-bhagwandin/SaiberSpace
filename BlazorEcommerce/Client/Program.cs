eee1c4b012c30bddc84528da4c2fc863e7c7af0a
Saieshen Bhagwandin <SaieshenB@hollywoodbets.net>
2022-08-17 13:55:47 +0200

    Added interface for cart

--- BlazorEcommerce/Client/Program.cs
+++ BlazorEcommerce/Client/Program.cs
@@ -3,11 +3,14 @@ global using System.Net.Http.Json;
 global using BlazorEcommerce.Client.Services.ProductService;
 global using BlazorEcommerce.Client.Services.CategoryService;
 global using BlazorEcommerce.Client.Services.UserService;
+global using BlazorEcommerce.Client.Services.CartService;
 global using Blazored.LocalStorage;
+using Blazored.Toast;
 using BlazorEcommerce.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.Components.Authorization;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -16,11 +19,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredToast();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider,CustomAuthStateProvider>();
+
 
 await builder.Build().RunAsync();
---
+++ BlazorEcommerce/Client/Services/CartService/CartService.cs
@@ -0,0 +1,87 @@
+﻿using Blazored.Toast.Services;
+
+namespace BlazorEcommerce.Client.Services.CartService
+{
+    public class CartService : ICartService
+    {
+        private readonly ILocalStorageService _localStorage;
+        private readonly IToastService _toastService;
+        private readonly IProductService _productService;
+
+        public CartService(ILocalStorageService localStorage,IToastService toastService,IProductService productService)
+        {
+            _localStorage = localStorage;
+            _toastService = toastService;
+            _productService = productService;
+        }
+
+
+        public event Action OnChange;
+
+        public async Task AddToCart(ProductVariant productVariant)
+        {
+            var cart = await _localStorage.GetItemAsync<List<ProductVariant>>("cart");
+
+            if (cart == null) {
+
+                cart = new List<ProductVariant>();
+
+            }
+
+            cart.Add(productVariant);   
+
+            await _localStorage.SetItemAsync("cart",cart);
+
+            var product = await _productService.GetProductByIdAsync(productVariant.ProductId);
+
+            _toastService.ShowSuccess(product.Data.Title, "Added to cart");
+
+            OnChange.Invoke();
+
+
+        }
+
+        public async Task<List<CartItem>> GetCartItems()
+        {
+            var result = new List<CartItem>();
+
+            var cart = await _localStorage.GetItemAsync<List<ProductVariant>>("cart");
+
+            if (cart == null) {
+
+                return result;
+
+            }
+
+            foreach (var item in cart) 
+            {
+                var product = await _productService.GetProductByIdAsync(item.ProductId);
+
+                var cartItem = new CartItem
+                {
+
+                    ProductId = product.Data.Id,
+                    ProductTitle = product.Data.Title,
+                    Image = product.Data.ImageUrl,
+                    EditionId = item.EditionId,
+
+                };
+
+                var variant = product.Data.Variants.Find(v => v.EditionId == item.EditionId);
+
+                if (variant != null) {
+
+                    cartItem.EditionName = variant.Edition?.Name;
+                    cartItem.Price = variant.Price;
+                }
+
+
+                result.Add(cartItem);
+
+            }
+
+            return result;
+
+        }
+    }
+}
