34f92e1689b1770fd80666d97c971e39f1493e84
Saieshen Bhagwandin <SaieshenB@hollywoodbets.net>
2022-08-25 10:40:01 +0200

    Added order amount of profile page and disabled text fields and added red text for discounts

---
+++ BlazorEcommerce/Shared/AccountDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorEcommerce.Shared
+{
+    public class AccountDetails
+    {
+
+        public UserRegisterRequest user { get; set; }
+
+        public List<Orders> Orders { get; set; }
+
+    
+
+
+    }
+}
